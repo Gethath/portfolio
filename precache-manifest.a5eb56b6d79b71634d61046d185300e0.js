f2931a2e164af97af871c39f47da93e553d62404
Gethath <danielkoskwork@gmail.com>
2020-04-04 20:59:21 +0200

    Updates

---
+++ precache-manifest.a5eb56b6d79b71634d61046d185300e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33efccbdad9da5f7ff874782dcce1ee4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f187f1eaa1f77a4ed6cb",
+    "url": "/portfolio/static/css/main.60dc1744.chunk.css"
+  },
+  {
+    "revision": "8be80feb701bcf65592d",
+    "url": "/portfolio/static/js/2.9343e8fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.9343e8fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f187f1eaa1f77a4ed6cb",
+    "url": "/portfolio/static/js/main.f861660e.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  }
+]);
