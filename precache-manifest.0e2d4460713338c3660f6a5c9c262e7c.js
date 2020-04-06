ce0ea40051462b43f24cc0fd086b9bf2acbe43a3
Gethath <danielkoskwork@gmail.com>
2020-04-06 23:09:16 +0200

    Updates

---
+++ precache-manifest.0e2d4460713338c3660f6a5c9c262e7c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5a007a1ab6ebd0e72bb7ee8b5e90dd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cd45549ba9fb4fed84e2",
+    "url": "/portfolio/static/css/main.82b6f2c9.chunk.css"
+  },
+  {
+    "revision": "40e98067ae26eb6ec89a",
+    "url": "/portfolio/static/js/2.39f513b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.39f513b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd45549ba9fb4fed84e2",
+    "url": "/portfolio/static/js/main.997e4c7c.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "58a075ecbad17bb815b0a5cb1780494f",
+    "url": "/portfolio/static/media/img-desktop.58a075ec.jpg"
+  },
+  {
+    "revision": "44e58fcbc4bfa94bfb9320fac74f5c08",
+    "url": "/portfolio/static/media/img-mobile-mini.44e58fcb.jpg"
+  },
+  {
+    "revision": "220aef060834c45df11449b9dbb1a0cd",
+    "url": "/portfolio/static/media/img-mobile.220aef06.jpg"
+  },
+  {
+    "revision": "340275d3dc66860b3d0420d75c5466ac",
+    "url": "/portfolio/static/media/img-tablet.340275d3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.12b104ea5c6b6615bed3ad3979324545.js"
+  "/portfolio/precache-manifest.0e2d4460713338c3660f6a5c9c262e7c.js"
 );
 
 self.addEventListener('message', (event) => {
